c3b817107d940d1a5cb5c3135f28884a604b718f
mi11km <mi11km.basket@gmail.com>
2022-07-02 04:54:37 +0900

    refactor retry

--- hello/retry.go
+++ hello/retry.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"flag"
-	"fmt"
 	"log"
 	"os/exec"
 	"time"
 )
 
 func main() {
-	waitTime := 3
+	waitTime := 10
 	counter := 0
 
 	flag.Parse()
@@ -20,30 +20,46 @@ Loop:
 	for {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(waitTime))
 		cmd := exec.CommandContext(ctx, args[0], args[1:]...)
+		var stdout bytes.Buffer
+		var stderr bytes.Buffer
+		cmd.Stdout = &stdout
+		cmd.Stderr = &stderr
+
 		err := cmd.Start()
 		if err != nil {
-			log.Println("Failed Start Command.")
-			continue Loop
+			log.Println("[RETRY] Failed Start Command.")
+			break Loop
 		}
+
 		counter += 1
-		log.Printf("Start Command(%d)\n", counter)
+		log.Printf("[RETRY] Start Command(%d)\n", counter)
+
 		go func(c int) {
 			if err = cmd.Wait(); err != nil {
-				log.Printf("Failed Run Command(%d): %v\n", c, err)
+				log.Printf("[RETRY] Failed Run Command(%d): %v\n", c, err)
+				if stderr.Len() != 0 {
+					log.Println(stderr.String())
+				}
 				return
 			}
 			isOK <- struct{}{}
 		}(counter)
+
 		for {
 			select {
 			case <-isOK:
 				cancel()
-				fmt.Println("Succeeded Command!")
+				log.Println("[RETRY] Succeeded Command!")
+				log.Println(stdout.String())
 				break Loop
 			case <-ctx.Done():
 				cancel()
-				log.Println("Failed: ", ctx.Err())
+				log.Println("[RETRY] Failed: ", ctx.Err())
 				continue Loop
+			default:
+				if stdout.Len() != 0 {
+					log.Println(stdout.String())
+				}
 			}
 		}
 	}
